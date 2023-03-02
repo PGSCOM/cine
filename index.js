d0875b0972fb49169820e2b0ce79755f7f539867
PGSCOM <69808296+PGSCOM@users.noreply.github.com>
2023-03-02 23:40:08 +0100

    Resolución variable en funcionamiento (por probar)

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const altura = screen.height;
 
-// Seleccionar el div por su identificador y cambiar su altura a 200 píxeles
-$('#video').height(`${altura}px`);
+while (true) {
+    $('#video').height(`${altura}px`);
+}
